73d28ad12068f6e76119f9c43eff4877216fe95a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-22 12:08:36 +0100

    learning to use javascript event listeners

--- js/main.js
+++ js/main.js
@@ -7,7 +7,13 @@ const h2 = div.querySelector("h2");
 //Syntax: addEventListener(event, function, useCapture)
 
 const doSomething = () => {
-  alert("doing somehting");
+  alert("doing something");
 };
 
 h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
+
+h2.addEventListener("click", (even) => {
+  console.log(even.target);
+  even.target.textContent = "clicked by MLO";
+});
